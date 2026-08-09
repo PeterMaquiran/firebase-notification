9184aeaa08328783f61a018670f88e20b06a1616
Peter Maquiran <maquiranpeter29@gmail.com>
2026-08-09 12:50:35 +0100

    add typscript

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import admin from 'firebase-admin';
+import admin, { type ServiceAccount } from 'firebase-admin';
+import type { Message } from 'firebase-admin/messaging';
 import { readFileSync } from 'node:fs';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -9,29 +10,40 @@ const __dirname = dirname(__filename);
 // Path to your Firebase service account JSON file
 const serviceAccount = JSON.parse(
   readFileSync(join(__dirname, 'serviceAccountKey.json'), 'utf8')
-);
+) as ServiceAccount;
 
 // Initialize the Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
+
 // The device registration token from the client FCM SDKs
-const registrationToken = 'dUahAEOM6Eo-jTAz9Vy6ZO:APA91bG5WVk1PrUi0g186UhZVRSjz9dgfPA4DVcvUw29kp3Tu_9BwSKUuWSesSlsx0KTKkBYbTGI8DYWGZWXkNkrLy4zdDHHUQ4iN3uW35JKzfecQW13sZE';
+const registrationToken =
+  'f6RnUAZijupKkpngRrJ7UP:APA91bFnYlxQLcO6vXZXK5NGCmuuQhi2rtEhH8g1LufGiNwqiTMRDa06ySzn6SjIFK-3rWh9rm04xAkFQ4xpg4Jm9lnEL71KYNqGE07Pc3rnpeHd9X4cbGY';
 
 // Define the notification message payload
-const message = {
+const message: Message = {
   notification: {
     title: 'Notification Title',
     body: 'This is the body of the notification.',
   },
-  token: registrationToken, // Target the specific device
+  webpush: {
+    notification: {
+      badge:
+        'https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/uptime-kuma.svg',
+      icon: 'https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/uptime-kuma.svg',
+    },
+  },
+  token: registrationToken,
 };
 
 // Send the message
-admin.messaging().send(message)
+admin
+  .messaging()
+  .send(message)
   .then((response) => {
     console.log('Successfully sent message:', response);
   })
-  .catch((error) => {
+  .catch((error: unknown) => {
     console.error('Error sending message:', error);
   });
